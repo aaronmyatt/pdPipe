2c44071934a83754e371c0edc80d36f3da5be4cf
Aaron Myatt <aaronmyatt@gmail.com>
2024-05-29 01:06:31 +0800

    adds and,or,not,flags handling

--- pdUtils.ts
+++ pdUtils.ts
@@ -1,25 +1,63 @@
-import type {Pipe, Input, Stage} from "./pipedown.d.ts";
+import type {Pipe, Input, Stage, Step} from "./pipedown.d.ts";
 import $p from "jsr:@pd/pointers@0.1.0";
 
 function funcWrapper<I extends Input>(funcs: Stage<I>[], opts: Pipe) {
   opts.$p = $p;
 
-  return funcs.map((func, index: number) => async function(input: I) {
-    const funcConfig = $p.get(opts, '/steps/' + index + '/config')
+  return funcs
+    // associate configuration with function
+    .map((func, index: number): { func: Stage, config: Step['config'] } => {
+      const config: Step['config'] = Object.assign(
+        { checks: [], not: [], or: [], and: [], routes: [], only: false, stop: false},
+        $p.get(opts, '/steps/' + index + '/config')
+      )
+      return { func, config }
+    })
+    // wrap function with configuration checks
+  .map(({func, config}, index) => async function(input: I) {
+    const only = config.only || input?.only
+    if(only && only !== index) return input
 
-    if(funcConfig && funcConfig.checks && funcConfig.checks.length > 0){
-      const checks = funcConfig.checks.reduce((acc: Pipe['checks'], check: string) => {
-        acc[check] = $p.get(input, check)
-        return acc
-      }, {} as Pipe['checks'])
-      opts.checks = checks
-      if(!Object.values(checks).some(check => !!check)){
-        return input
+    const stop = config.stop || input?.stop
+    if(index > stop) return input;
+
+    if (input?.errors && input.errors.length > 0) return input;
+
+
+    // if check is truthy, bail early
+    const shouldBeFalsy = config.not.map((check: string) => $p.get(input, check)).some((check: boolean) => check)
+    if(shouldBeFalsy) return input;
+
+    /**
+     * With no 'and' or 'or' conditions, any 'checks' that are truthy will run the function
+     * any 'and' conditions that are falsy will not run the function
+     * any 'or' conditions that are truthy will run the function
+    */
+
+    const checker = (check: string): string[] => {
+      return [check.split('/').pop() || check, $p.get(input, check)]
+    }
+
+    const validator: 'some'|'every' = config.and.length ? 'every' : 'some';
+    const conditions: string[][] = config.checks.map(checker).concat(config.and.map(checker))
+    const orConditions = config.or.map(checker)
+    
+    if(conditions.length){
+      const firstChecks = conditions[validator](([_key,value]) => !!value)
+      const orChecks = orConditions.some(([_key,value]: string[]) => !!value)
+
+      if(firstChecks){
+        $p.set(opts, '/checks', Object.fromEntries(conditions));
+      } 
+      else if(orChecks) {
+         $p.set(opts, '/checks', Object.fromEntries(orConditions));
+      } else {
+        return input;
       }
     }
 
-    if(funcConfig && funcConfig.routes && input.request){
-      const route = funcConfig.routes
+    if(config.routes && input.request){
+      const route = config.routes
       .map((route: string) => new URLPattern({ pathname: route }))
       .find((route: URLPattern) => {
         return route.test(input.request.url);
@@ -30,20 +68,6 @@ function funcWrapper<I extends Input>(funcs: Stage<I>[], opts: Pipe) {
       }
 
       input.route = route.exec(input.request.url);
-    }
-
-    const only = (funcConfig && funcConfig.only) || input.only
-    if(only && only !== index){
-      return input
-    }
-
-    const stop = (funcConfig && funcConfig.stop) || input.stop
-    if(index > stop){
-      return input
-    }
-
-    if (input.errors && input.errors.length > 0) {
-      return input;
     }
 
     try {
@@ -67,4 +91,4 @@ function funcWrapper<I extends Input>(funcs: Stage<I>[], opts: Pipe) {
 }
 
 export { funcWrapper };
-export default funcWrapper
+export default funcWrapper;
